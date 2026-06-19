e89379287f9b893846b0387686db4cf090cd48b4
Hasib <hasibhsb19@gmail.com>
2026-06-19 18:16:50 +0600

    add createPrompt action to save prompt data in MongoDB with reusable helpers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 // Load environment variables
 dotenv.config();
@@ -12,12 +12,69 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Connect to DB
+    const db = client.db(process.env.DB_NAME);
+
+    // Create or Access to DB Collections
+    const promptCollection = db.collection("prompts");
+
+    // ====================  Prompts  ====================
+
+    // Insert New Created Prompt Data on 'MongoDB'
+    app.post("/api/prompts", async (req, res) => {
+      try {
+        const prompt = req.body;
+
+        const promptData = {
+          ...prompt,
+          createdAt: new Date(),
+        };
+
+        const result = await promptCollection.insertOne(promptData);
+        res.status(201).send(result);
+      } catch (err) {
+        res.status(500).send({
+          success: false,
+          message: "Internal Server Error. Something went wrong!",
+          error: err.message,
+        });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Base Health Check Route
-app.get('/', (req, res) => {
-    res.send("Backend server is running successfully!");
+app.get("/", (req, res) => {
+  res.send("Backend server is running successfully!");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
